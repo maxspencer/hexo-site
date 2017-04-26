c7ce1ceed82d43b0a49f91a85fa76d0319203ca2
Max Spencer <mrmaxspencer@googlemail.com>
2017-04-26 23:29:31 +0100

    Use lambdeploy 0.1.2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var lambdeploy = require('lambdeploy');
 
 exports.handler = lambdeploy.createHandler(function(workingDir, callback) {
+    var path = require('path');
     var Hexo = require('hexo');
     var hexo = new Hexo(workingDir, {});
     hexo.init({}).then(function() {
-	hexo.call('generate', {}).then(callback).catch(callback);
+	hexo.call('generate', {}).then(function() {
+	    callback(null, path.join(workingDir, 'build'));
+	}).catch(callback);
     }).catch(callback);
 });
